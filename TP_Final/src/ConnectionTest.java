d57b5bd24b27aecd550b979247237cd2325a8644
Gian Pelegrini <62160448+gianpelegrini@users.noreply.github.com>
2021-01-27 21:32:11 -0300

    Add files via upload

---
+++ TP_Final/src/ConnectionTest.java
@@ -0,0 +1,12 @@
+//Giancarlo Pelegrini CB3007634
+
+import java.sql.Connection; 
+import java.sql.SQLException; 
+
+public class ConnectionTest {     
+    public static void main(String[] args) throws SQLException {
+         Connection connection = new JavaConnect().getConnection();
+         System.out.println("Connection working!");
+         connection.close();
+     }
+}
